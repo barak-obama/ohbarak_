3fdc4c93f568763c1a0fea3cb1b84c5e3a689977
Barak Obama <barak.oh16@gmail.com>
2018-05-12 17:22:02 +0300

    dsfsedfsdf

--- public/scripts/audio_play.js
+++ public/scripts/audio_play.js
@@ -1,72 +1,65 @@
-
-
 var ohbaraks = [];
 const ohbaraksUrls = {};
 const ohbaraksAudios = {};
+const all_ohbaraks = [];
+var current_ohbarak = null;
 
 const storageRef = firebase.storage().ref();
 
 
+nsfw_checkbox = document.getElementById("nsfw");
 
 
 
-function getApprovedTracks(){
+function filter_ohbaraks(){
+    if (nsfw_checkbox.checked) {
+        ohbaraks = all_ohbaraks;
+    } else {
+        ohbaraks = all_ohbaraks.filter(name => !name.includes("nsfw"));
+    }
+}
+
+
+function getApprovedTracks() {
     const starCountRef = firebase.database().ref('approved');
 
-    starCountRef.on('value', function(audio_files) {
+    starCountRef.on('value', function (audio_files) {
 
         audio_files.forEach(function (file) {
             const file_name = file.val();
             const path = "approved/" + file_name;
 
-            storageRef.child(path).getDownloadURL().then(function(url){
-                ohbaraksUrls[file_name] = url;
-                ohbaraks.push(file_name);
-            }).catch(function(error) {
-                console.error(error);
-            });
-
+            var url = storageRef.child(path).getDownloadURL();
+            ohbaraksUrls[file_name] = url;
+            ohbaraks.push(file_name);
+            all_ohbaraks.push(file_name)
         });
+
+        filter_ohbaraks();
     });
 }
 
-getApprovedTracks();
 
+document.getElementById("ohbarak").addEventListener("click", function () {
+    const ohbarak = ohbaraks[Math.floor(Math.random() * ohbaraks.length)];
 
-
-
-
-
-
-
-const all_ohbaraks = ohbaraks;
-let current_ohbarak;
-
-
-
-document.getElementById("ohbarak").addEventListener("click", function() {
-    const ohbarak = ohbaraks[Math.floor(Math.random()*ohbaraks.length)];
-
-    if(current_ohbarak){
+    if (current_ohbarak) {
         current_ohbarak.pause();
         current_ohbarak.currentTime = 0;
     }
 
-    if(ohbaraksAudios[ohbarak]){
+    if (ohbaraksAudios[ohbarak]) {
         ohbaraksAudios[ohbarak].currentTime = 0;
         ohbaraksAudios[ohbarak].play();
-    }else {
-        ohbaraksAudios[ohbarak] = new Audio(ohbaraksUrls[ohbarak]);
+    } else {
+        ohbaraksAudios[ohbarak] = new Audio(ohbaraksUrls[ohbarak]["i"]);
         current_ohbarak = ohbaraksAudios[ohbarak];
         ohbaraksAudios[ohbarak].play();
     }
 });
 
-document.getElementById("nsfw").addEventListener("change", function() {
-    if (this.checked)
-        ohbaraks = all_ohbaraks;
-    else
-        ohbaraks = all_ohbaraks.filter(name => !name.includes("nsfw"));
 
-    console.log(ohbaraks);
-});
+nsfw_checkbox.addEventListener("change", filter_ohbaraks);
+
+
+getApprovedTracks();
